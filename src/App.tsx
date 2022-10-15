767282ed289beaea97555a7e3c24fb3c089bc795
Shane <shane@swalker.dev>
2022-10-15 08:50:22 -0500

    added charka ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import {
+  Box,
+  Button,
+  Code,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Text
+} from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+    <Container textAlign={"center"} mt={14} maxW={"container.xl"}>
+      <Flex justifyContent={"center"}>
+        <Link href="https://vitejs.dev" target="_blank">
+          <Image src="/vite.svg" alt="Vite logo" />
+        </Link>
+        <Link href="https://reactjs.org" target="_blank">
+          <Image src={reactLogo} alt="React logo" />
+        </Link>
+      </Flex>
+      <Heading textAlign={"center"} as={"h1"} fontWeight={"light"} size="4xl">
+        Vite + React
+      </Heading>
+      <Box className="card">
+        <Button
+          bg={"blue.400"}
+          color={"whiteAlpha.700"}
+          variant={"solid"}
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count: {count}
+        </Button>
+        <Text>
+          Edit <Code colorScheme="blue">src/App.tsx</Code> and save to
+          test HMR
+        </Text>
+      </Box>
+      <Text as="sub" color="lightgray">
         Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      </Text>
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById("root") as HTMLElement;
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
-)
+);
